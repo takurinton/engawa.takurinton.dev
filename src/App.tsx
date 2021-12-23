ea5c1fc50d9dc108d8f9dfde1c5713e6cefc9887
takurinton <takurinton@takurinton.com>
2021-12-24 08:39:52 +0900

    add: keydown

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { Box } from "@chakra-ui/react"
+import { useCallback, useEffect } from "react";
+
 export const App = () => {
   return (
-    <>hello react!!!</>
+    <Box>
+      <SushiModal />
+    </Box>
+  )
+}
+
+const SushiModal = () => {
+  const handleKeyDown = useCallback((event) => {
+    console.log(event.key);
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown);
+    return () => document.removeEventListener('keydown', handleKeyDown);
+  }, []);
+
+  return (
+    <Box>
+      shshi
+    </Box>
   )
 }
